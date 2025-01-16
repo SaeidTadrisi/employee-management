42ebeb984ab1289b19db8249ea5fce4e2a5724df
Saeid.Tadrisi <saeid.tadrisi@gmail.com>
2025-01-16 21:08:58 +0100

    Department and Position assigns methods added.

--- src/main/java/com/example/employee/employee_management/service/EmployeeService.java
+++ src/main/java/com/example/employee/employee_management/service/EmployeeService.java
@@ -1,7 +1,11 @@
 package com.example.employee.employee_management.service;
 
+import com.example.employee.employee_management.model.Department;
 import com.example.employee.employee_management.model.Employee;
+import com.example.employee.employee_management.model.Position;
+import com.example.employee.employee_management.repository.DepartmentRepository;
 import com.example.employee.employee_management.repository.EmployeeRepository;
+import com.example.employee.employee_management.repository.PositionRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,9 +14,15 @@ import java.util.Optional;
 public class EmployeeService {
 
     EmployeeRepository employeeRepository;
+    DepartmentRepository departmentRepository;
+    PositionRepository positionRepository;
 
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    public EmployeeService(EmployeeRepository employeeRepository
+            , DepartmentRepository departmentRepository
+            , PositionRepository positionRepository) {
         this.employeeRepository = employeeRepository;
+        this.departmentRepository = departmentRepository;
+        this.positionRepository = positionRepository;
     }
 
     public Employee createEmployee(Employee employee){
@@ -38,4 +48,28 @@ public class EmployeeService {
         employeeRepository.deleteById(id);
     }
 
+    public Employee assignDepartment(Long employeeId, Long departmentId){
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new RuntimeException("Employee not found with id: " + employeeId));
+
+
+        Department department = departmentRepository.findById(departmentId)
+                .orElseThrow(() -> new RuntimeException("Department not found with id: " + departmentId));
+
+        employee.setDepartment(department);
+
+        return employeeRepository.save(employee);
+    }
+
+    public Employee assignPosition(Long employeeId, Long positionId){
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new RuntimeException("Employee not found with id: " + employeeId));
+
+        Position position = positionRepository.findById(positionId)
+                .orElseThrow(() -> new RuntimeException("Position not found with id: " + positionId));
+
+        employee.setPosition(position);
+
+        return employeeRepository.save(employee);
+    }
 }
