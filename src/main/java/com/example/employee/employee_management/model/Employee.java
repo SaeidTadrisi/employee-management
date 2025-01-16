297ebec549eabfe70d315fa8b0a4debe10d8c771
Saeid.Tadrisi <saeid.tadrisi@gmail.com>
2025-01-16 13:55:39 +0100

    model classes constructors and getters/setters updated.

--- src/main/java/com/example/employee/employee_management/model/Employee.java
+++ src/main/java/com/example/employee/employee_management/model/Employee.java
@@ -22,5 +22,62 @@ public class Employee {
 
     private Double salary;
 
+    public Employee() {
+    }
 
+    public Employee(String name, Double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", position=" + position +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
 }
--- src/main/java/com/example/employee/employee_management/model/Position.java
+++ src/main/java/com/example/employee/employee_management/model/Position.java
@@ -19,4 +19,54 @@ public class Position {
     @OneToMany(mappedBy = "position",
             cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private List<Employee> employees;
+
+    public Position() {
+    }
+
+    public Position(String responsibilities, String title) {
+        this.responsibilities = responsibilities;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public String getResponsibilities() {
+        return responsibilities;
+    }
+
+    public void setResponsibilities(String responsibilities) {
+        this.responsibilities = responsibilities;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", responsibilities='" + responsibilities + '\'' +
+                ", employees=" + employees +
+                '}';
+    }
 }
